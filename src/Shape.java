cb720770b0a35bae7b65352d025a61bd09bf435e
Hepheir <hepheir@gmail.com>
2023-11-30 20:04:05 +0900

    feat(Shape.java): AbstractSelectable 을 상속하여 일부 Selectable을 구현한 클래스들 간에 중복된 메소드의 구현을 생략

--- src/Shape.java
+++ src/Shape.java
@@ -1,36 +1,5 @@
-public class Shape implements Selectable {
-    @Override
-    public int getMinBoundsX() {
-        return 0;
-    }
-
-    @Override
-    public int getMinBoundsY() {
-        return 0;
-    }
-
-    @Override
-    public int getMaxBoundsX() {
-        return 0;
-    }
-
-    @Override
-    public int getMaxBoundsY() {
-        return 0;
-    }
-
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public boolean isSelected(int x, int y) {
-        return false;
-    }
-
-    @Override
-    public void print() {
-
+public class Shape extends AbstractSelectable {
+    public Shape(String name) {
+        super(name);
     }
 }
