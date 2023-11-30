c2be76bca422b89085fd2163b2e3a86d41ffa972
Hepheir <hepheir@gmail.com>
2023-11-30 21:40:54 +0900

    refactor(Shape.java): Shape의 print() 메소드 구현

--- src/Shape.java
+++ src/Shape.java
@@ -2,4 +2,11 @@ public class Shape extends AbstractSelectable {
     public Shape(String name) {
         super(name);
     }
+
+    @Override
+    public void print() {
+        System.out.printf("%s:\n", getName());
+        System.out.printf("minBoundsX: %d, minBoundsY: %d\n", getMinBoundsX(), getMinBoundsY());
+        System.out.printf("maxBoundsX: %d, maxBoundsY: %d\n", getMaxBoundsX(), getMaxBoundsY());
+    }
 }
