2423211bd8f52fff958f76fba838737e597dd423
Hepheir <hepheir@gmail.com>
2023-11-30 21:43:29 +0900

    feat(Group.java): add() 메소드와 List<Selectable> selectables 멤버 변수를 추가
    Group이 Composite 역할을 수행하도록 함

--- src/Group.java
+++ src/Group.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group extends AbstractSelectable {
+    private List<Selectable> selectables;
+
     public Group(int num) {
         super("Group: " + num);
+        selectables = new ArrayList<>();
+    }
+
+    public void add(Selectable o) {
+        selectables.add(o);
+        minBoundsX = Math.min(minBoundsX, o.getMinBoundsX());
+        minBoundsY = Math.min(minBoundsY, o.getMinBoundsY());
+        maxBoundsX = Math.max(maxBoundsX, o.getMinBoundsX());
+        maxBoundsY = Math.max(maxBoundsY, o.getMinBoundsY());
     }
 }
